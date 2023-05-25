20bafd9110a4dfc7bbe09a177a3bf406509215e9
plhacko <plhacko@gmail.com>
2023-05-25 19:28:44 +0200

    wip - EnemyDetector

--- ParkGame/Assets/Scripts/Units/EnemyDetector.cs
+++ ParkGame/Assets/Scripts/Units/EnemyDetector.cs
@@ -1,23 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyDetector : MonoBehaviour
 {
-    public List<GameObject> soldiersInMyVicinity;
+    public List<Soldier> enemySoldiersInMyVicinity = new List<Soldier>();
+    private Soldier localSoldier;
 
-    private void Awake() {
-        soldiersInMyVicinity = new List<GameObject>();
+    private void Awake()
+    {
+        localSoldier = GetComponentInParent<Soldier>();
     }
-    private void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log($"utululululu {other.gameObject.name}");
-        GameObject otherSoldier = other.gameObject;
-        // ptej se na tym
-        soldiersInMyVicinity.Add(otherSoldier);
+
+    public Soldier GetClosesEnemy()
+    {
+        Soldier closestEnemy = null;
+        float closestDistance = float.MaxValue;
+        foreach (Soldier e in enemySoldiersInMyVicinity)
+        {
+            float distance = Vector3.Distance(e.transform.position, localSoldier.transform.position);
+            if (closestDistance > distance)
+            { 
+                closestEnemy = e;
+                closestDistance = distance;
+            }
+        }
+
+        return closestEnemy;
     }
-    private void OnTriggerExit2D(Collider2D other) {
-        Debug.Log("TULUU!");
-        GameObject s = other.gameObject;
-        soldiersInMyVicinity.Remove(s);
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log($"triger ENTER {other.gameObject.name}");
+
+        Soldier otherSoldier = other.gameObject.GetComponent<Soldier>();
+
+        // checks if the soldier should be added
+        if (otherSoldier == null
+            || otherSoldier.team == localSoldier.team
+            || enemySoldiersInMyVicinity.Contains(otherSoldier))
+        { return; }
+
+        enemySoldiersInMyVicinity.Add(otherSoldier);
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log($"triger EXIT {other.gameObject.name}");
+
+        // removes the soldier from a list, because it is no longer visible
+        Soldier otherSoldier = other.gameObject.GetComponent<Soldier>();
+        if (otherSoldier == null) { return; }
+        enemySoldiersInMyVicinity.Remove(otherSoldier);
     }
 }
