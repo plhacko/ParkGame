016edb66c8033063aec41bd88330b642790e608c
Hong Son Ngo <toilahongson@gmail.com>
2023-11-20 12:50:22 +0100

    Misc UI Pages

---
+++ ParkGame/Assets/UI/Pages/How To Play Screen/UIHowToPlayScreenController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIHowToPlayScreenController : UIPageController
+{
+    public override void OnEnter()
+    {}
+
+    public override void OnExit()
+    {}
+}
---
+++ ParkGame/Assets/UI/Pages/Options Screen/UIOptionsScreenController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIOptionsScreenController : UIPageController
+{
+    public override void OnEnter()
+    {}
+
+    public override void OnExit()
+    {}
+}
