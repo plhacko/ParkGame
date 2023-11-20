2aa872997ece096409a72c58d6f6b8d37f63908a
Hong Son Ngo <toilahongson@gmail.com>
2023-11-20 12:50:22 +0100

    Misc UI Pages

---
+++ ParkGame/Assets/UI/Pages/About Screen/UIAboutScreenController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIAboutScreenController : UIPageController
+{
+    public override void OnEnter()
+    {}
+
+    public override void OnExit()
+    {}
+}
