ab27c70b41a2393104485446e2c083b9fb0c54bd
Hong Son Ngo <toilahongson@gmail.com>
2023-11-21 16:56:10 +0100

    Adding how to play and options screen to game scene

--- ParkGame/Assets/UI/Pages/How To Play Screen/UIHowToPlayScreenController.cs
+++ ParkGame/Assets/UI/Pages/How To Play Screen/UIHowToPlayScreenController.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIHowToPlayScreenController : UIPageController
 {
+    [SerializeField] private Button backButton;
+
+    private void Awake()
+    {
+        backButton.onClick.AddListener(Back);
+    }
+
+    private void Back()
+    {
+        UIController.Singleton.PopUIPage();
+    }
+
     public override void OnEnter()
     {}
 
--- ParkGame/Assets/UI/Pages/In Game Screen/Scripts/UIInGameScreenController.cs
+++ ParkGame/Assets/UI/Pages/In Game Screen/Scripts/UIInGameScreenController.cs
@@ -1,9 +1,66 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIInGameScreenController : UIPageController
 {
+    [SerializeField] private Button optionsButton;
+    [SerializeField] private Button mapButton;
+    [SerializeField] private Button followButton;
+    [SerializeField] private Button cameraButton;
+    [SerializeField] private Button action1;
+    [SerializeField] private Button action2;
+    [SerializeField] private Button action3;
+    [SerializeField] private UIPage optionsPage;
+
+    private void Awake()
+    {
+        optionsButton.onClick.AddListener(Options);
+        mapButton.onClick.AddListener(Map);
+        followButton.onClick.AddListener(Follow);
+        cameraButton.onClick.AddListener(CameraResize);
+        action1.onClick.AddListener(Action1);
+        action2.onClick.AddListener(Action2);
+        action3.onClick.AddListener(Action3);
+    }
+
+    private void Action3()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Action2()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Action1()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void CameraResize()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Follow()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Map()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Options()
+    {
+        UIController.Singleton.PushUIPage(optionsPage);
+    }
+
     public override void OnEnter()
     {
     }
