253b7cc4b98837700a2e358a258cb14347ee2ae9
Hong Son Ngo <toilahongson@gmail.com>
2023-11-21 16:56:10 +0100

    Adding how to play and options screen to game scene

---
+++ ParkGame/Assets/FitViewToGameUI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.U2D.Animation;
+using UnityEngine;
+
+public class FitViewToGameUI : MonoBehaviour
+{
+    [SerializeField] private RectTransform gameView;
+
+    void Awake()
+    {
+        var gameViewHeight = gameView.anchorMin.y;
+        Camera.main.rect = new Rect(0, gameViewHeight, 1, 1 - gameViewHeight);
+    }
+}
--- ParkGame/Assets/UI/Pages/Options Screen/UIOptionsScreenController.cs
+++ ParkGame/Assets/UI/Pages/Options Screen/UIOptionsScreenController.cs
@@ -1,12 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIOptionsScreenController : UIPageController
 {
+    [SerializeField] private Button backButton;
+    [SerializeField] private Button quitButton;
+    [SerializeField] private Button backgroundNoiseButton;
+    [SerializeField] private Button mainMusicButton;
+    [SerializeField] private Button howToPlayButton;
+    [SerializeField] private UIPage howToPlayPage;
+    [SerializeField] private string mainMenuSceneName = "Menu";
+
+    private void Awake()
+    {
+        backButton.onClick.AddListener(Back);
+        quitButton.onClick.AddListener(Quit);
+        backgroundNoiseButton.onClick.AddListener(ToggleBackgroundNoise);
+        mainMusicButton.onClick.AddListener(ToggleMainMusic);
+        howToPlayButton.onClick.AddListener(HowToPlay);        
+
+    }
+
     public override void OnEnter()
     {}
 
     public override void OnExit()
     {}
+
+    private void Back()
+    {
+        UIController.Singleton.PopUIPage();
+    }
+
+    private void Quit()
+    {
+        // TODO handle disconnecting from server
+        SceneManager.LoadScene(mainMenuSceneName);
+    }
+
+    private void ToggleBackgroundNoise()
+    {
+        // TODO toggle background noise
+    }
+
+    private void ToggleMainMusic()
+    {
+        // TODO toggle main music
+    }
+
+    private void HowToPlay()
+    {
+        UIController.Singleton.PushUIPage(howToPlayPage);
+    }
 }
