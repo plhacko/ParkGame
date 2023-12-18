7b04167356a66b1e1359b6fb2a56c6f3b2859cd3
beta_caroten <abi@mrq.cz>
2023-12-18 11:22:28 +0100

    structure sfx in audiomanager
    - lists for randomization of some sound effects (attacks, death)
    - just structure for now, without the sound clips

--- ParkGame/Assets/Sound/AudioManager.cs
+++ ParkGame/Assets/Sound/AudioManager.cs
@@ -13,10 +13,22 @@ public class AudioManager : MonoBehaviour
     }
 
     public static AudioManager Instance;
-    [SerializeField]
-    private AudioSource sfxSource;
-    [SerializeField]
-    private List<Sound> sfxList;
+
+    [SerializeField] private AudioSource sfxSource;
+
+    // formation fanfares: free, circle, box, attack, fallback
+    // UI sfx: click on button. start game button (?)
+    // notification sfx: {3. 2. 1. start game}, opening VP, got VP, got outpost, lost outpost
+    // chimes: won game, lost game
+    [Tooltip("{sfxName, clip}")] 
+    [SerializeField] private List<Sound> sfxList;
+
+    [SerializeField] private List<AudioClip> pawnAttackSfx_list;
+    [SerializeField] private List<AudioClip> archerAttackSfx_list;
+    [SerializeField] private List<AudioClip> molemanAttackSfx_list;
+    [SerializeField] private List<AudioClip> diedSfx_list;
+
+    private Dictionary<string, AudioClip> sfxDict;
     private bool sfxMute;
 
     private void Awake() {
@@ -28,17 +40,43 @@ public class AudioManager : MonoBehaviour
         }
     }
 
+    private void Start() {
+        foreach (Sound s in sfxList) {
+            sfxDict[s.name] = s.sound;
+        }
+    }
+
     public void PlaySFX(AudioClip sfx) {
         sfxSource.PlayOneShot(sfx);
     }
 
     public void PlaySFX(string sfxName) {
-        foreach (Sound s in sfxList) {
-            if (s.name == sfxName) {
-                sfxSource.PlayOneShot(s.sound);
-                return;
-            }
-        }
+        sfxSource.PlayOneShot(sfxDict[sfxName]);
+    }
+
+    private AudioClip GetRandomItem(List<AudioClip> lst) {
+        int idx = UnityEngine.Random.Range(0, lst.Count);
+        return lst[idx];
+    }
+
+    public void PlayPawnAttack() {
+        AudioClip sfx = GetRandomItem(pawnAttackSfx_list);
+        sfxSource.PlayOneShot(sfx);
+    }
+
+    public void PlayArcherAttack() {
+        AudioClip sfx = GetRandomItem(archerAttackSfx_list);
+        sfxSource.PlayOneShot(sfx);
+    }
+
+    public void PlayMolemanAttack() {
+        AudioClip sfx = GetRandomItem(molemanAttackSfx_list);
+        sfxSource.PlayOneShot(sfx);
+    }
+
+    public void PlayDead() {
+        AudioClip sfx = GetRandomItem(diedSfx_list);
+        sfxSource.PlayOneShot(sfx);
     }
 
     public void ChangeSfxVolume(float volume) {
