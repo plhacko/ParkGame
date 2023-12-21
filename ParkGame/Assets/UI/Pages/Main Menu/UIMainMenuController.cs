7669c7b22b4a6aa4a22198402eb05c0f8946b21a
ngohongs <toilahongson@gmail.com>
2023-12-21 12:12:47 +0100

    Adding animations for main menu

--- ParkGame/Assets/UI/Pages/Main Menu/UIMainMenuController.cs
+++ ParkGame/Assets/UI/Pages/Main Menu/UIMainMenuController.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 using Managers;
 using Firebase.Auth;
 using Unity.Services.Authentication;
+using DG.Tweening;
 
 public class UIMainMenuController : UIPageController
 {
@@ -14,6 +15,10 @@ public class UIMainMenuController : UIPageController
     [SerializeField] private Button joinButton;
     [SerializeField] private Button aboutButton;
     [SerializeField] private Button signOutButton;
+    [SerializeField] private RectTransform headerPanel;
+    private Vector2 finalHeaderPanelPos;
+    [SerializeField] private RectTransform buttonsPanel;
+    private Vector2 finalButtonsPanelPos;
     [SerializeField] private UIPage welcomePage;
     [SerializeField] private UIPage lobbyPage;
     [SerializeField] private UIPage prepareGamePage;
@@ -31,6 +36,11 @@ public class UIMainMenuController : UIPageController
 
         // TODO remove this
         joinCodeInputField.text = PlayerPrefs.GetString("DebugRoomCode", "");
+
+        finalHeaderPanelPos = headerPanel.anchoredPosition;
+        finalButtonsPanelPos = buttonsPanel.anchoredPosition;
+
+        Prepare();
     }
 
     private void OnDestroy()
@@ -40,10 +50,15 @@ public class UIMainMenuController : UIPageController
     public override void OnEnter()
     {
         enableButtons(true);
+
+        float animationLength = .5f;
+        headerPanel.DOAnchorPosY(finalHeaderPanelPos.y, animationLength).SetEase(Ease.OutQuart);
+        buttonsPanel.DOAnchorPosY(finalButtonsPanelPos.y, animationLength).SetEase(Ease.OutQuart);
     }
 
     public override void OnExit()
     {
+        Prepare();
     }
     
     private void Create()
@@ -87,5 +102,23 @@ public class UIMainMenuController : UIPageController
         joinCodeInputField.interactable = isInteractable;
         joinButton.interactable = isInteractable;
         signOutButton.interactable = isInteractable;
+    }
+
+    private void Prepare()
+    {
+        var headerPivot = headerPanel.pivot.y;
+        var headerHeight = headerPanel.rect.height;
+
+        var headerOffset = headerHeight * headerPivot;
+
+        headerPanel.anchoredPosition = new Vector2(0, headerOffset);
+
+        // Get the height of the buttonsPanel
+        float buttonsPanelHeight = buttonsPanel.rect.height;
+        var entirePanel = GetComponent<RectTransform>();
+        var btnBottomPadding = buttonsPanel.anchorMin.y * entirePanel.rect.height; 
+
+        // Set the y-coordinate of the buttonsPanel's anchoredPosition to be the negative of its own height
+        buttonsPanel.anchoredPosition = new Vector2(0, -buttonsPanelHeight - btnBottomPadding);
     }
 }
